50071212e80902ae771b9db1bc2c55dcc0e31b71
ItzelQuintana <ovalli0326@gmail.com>
2023-09-15 11:45:10 -0600

    Primer intento manejo del DOM

--- src/view.js
+++ src/view.js
@@ -1,6 +1,9 @@
-// estas funciones son de ejemplo
+//debe tener todas las funciones que utilizara para renderizar los elementos dinámicamente.
+//Función obligatoria:renderItems(data): esta función recibe el arreglo de data para renderizar los elementos de cada item, y debería volver un elemento DOM o un string de HTML.
 
 export const renderItems = () => {
+    
   return 'example';
-};
+  }
+
 
