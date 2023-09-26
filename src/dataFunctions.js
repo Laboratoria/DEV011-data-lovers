c8f88bd1ce22871b342ae5cfde24b3e3997b9df0
ItzelQuintana <ovalli0326@gmail.com>
2023-09-25 19:11:28 -0600

    fOrdenar terminada

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,13 +1,20 @@
 // Debe contener toda la funcionalidad que corresponda a obtener, procesar y manipular datos (tus funciones)
 
-export const ordenar = (dataReleaseDate) => {
- dataReleaseDate.sort()
-  
-  
+export const example = () => {
+  function sortData(data, sortBy, sortOrder){
+    
 
+
+
+ return 'example';
 };
+
+}
 
 export const anotherExample = () => {
   return [];
 };
+
+
+
 
--- src/main.js
+++ src/main.js
@@ -1,17 +1,34 @@
 //Código que tenga que ver con mostrar los datos en la pantalla. Interacción del DOM. Llamas a las funciones de dataFunctions.js
 
-import { ordenar } from "./dataFunctions.js";
+import { example } from "./dataFunctions.js";
 import { renderItems } from "./view.js";
 import data from "./data/ghibli/ghibli.js";
 
-//Creación original de tarjetas
-renderItems(data)
 
-//Función para ordenar
-const dataReleaseDate = data.films.map((film) => film.release_date);
+//Mostar películas desordenadas
+let peliculas = data.films
+renderItems(peliculas)
 
-console.log(dataReleaseDate)
-ordenar(dataReleaseDate)
+//Función para ordenar y mostar las películas
+      
+const btnOrden = document.querySelector("select");
+btnOrden.addEventListener("change", function() {
+    const selectedValue = btnOrden.value;
+    console.log (selectedValue);
+
+  if (selectedValue === "asc") {
+    peliculas = data.films.sort((a,b)=>new Date(a.release_date) - new Date(b.release_date));
+   }
+    else if (selectedValue === "desc") {
+    peliculas = data.films.sort((a,b)=>new Date(b.release_date) - new Date(a.release_date));
+    }
+
+renderItems(peliculas);
+});
+
+
+
+
 
 
 
