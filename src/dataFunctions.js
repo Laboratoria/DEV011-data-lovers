a05b6779880b7066f2ce20fbb8af02f6efdf2f22
Carolina <caroonava21@gmail.com>
2023-09-29 13:42:00 -0300

    Cambios filter, btn clear y test

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -3,11 +3,15 @@
 //sortData = (data, sortBy, sortOrder)
 export const sortData = (data, sortBy, sortOrder) => {
   let yearFILMS = [];
-
+  const dataClone = [...data];
   if (sortOrder === "asc") {
-    yearFILMS = data.sort((a, b) => new Date(a[sortBy]) - new Date(b[sortBy]));
+    yearFILMS = dataClone.sort(
+      (a, b) => new Date(a[sortBy]) - new Date(b[sortBy])
+    );
   } else if (sortOrder === "desc") {
-    yearFILMS = data.sort((a, b) => new Date(b[sortBy]) - new Date(a[sortBy]));
+    yearFILMS = dataClone.sort(
+      (a, b) => new Date(b[sortBy]) - new Date(a[sortBy])
+    );
   }
 
   return yearFILMS;
@@ -18,31 +22,8 @@ export const sortData = (data, sortBy, sortOrder) => {
 export const filterData = (data, filterBy, value) => {
   let directorFILMS = [];
 
-  if (value === "Hayao") {
-    directorFILMS = data.filter(
-      (director) => director[filterBy] === "Hayao Miyazaki"
-    );
-  } else if (value === "Isao") {
-    directorFILMS = data.filter(
-      (director) => director[filterBy] === "Isao Takahata"
-    );
-  } else if (value === "Yoshifumi") {
-    directorFILMS = data.filter(
-      (director) => director[filterBy] === "Yoshifumi Kondō"
-    );
-  } else if (value === "Hiroyuki") {
-    directorFILMS = data.filter(
-      (director) => director[filterBy] === "Hiroyuki Morita"
-    );
-  } else if (value === "Gorō") {
-    directorFILMS = data.filter(
-      (director) => director[filterBy] === "Gorō Miyazaki"
-    );
-  } else if (value === "Hiromasa") {
-    directorFILMS = data.filter(
-      (director) => director[filterBy] === "Hiromasa Yonebayashi"
-    );
-  }
+  value === "Hayao Miyazaki";
+  directorFILMS = data.filter((director) => director[filterBy] === value);
 
   return directorFILMS;
 };
--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -3,8 +3,8 @@ import { filterData } from "../src/dataFunctions.js";
 import { dataTrue, data as fakeData } from "./data.js";
 
 describe("filterData", () => {
-  it("Filtar los directores por edad", () => {
-    const dataFiltered = filterData(fakeData["personas"], "edad", "23");
-    expect(dataFiltered).toBe(dataTrue["personas"]);
+  it("Filtar los directores por nombre", () => {
+    const dataFiltered = filterData(fakeData["personas"], "director", "Hayao");
+    expect(dataFiltered).toEqual(dataTrue["personas"]);
   });
 });
