313645e97cb7d52e92808b6c0457b1014f662531
ItzelQuintana <ovalli0326@gmail.com>
2023-10-02 11:42:37 -0600

    Arreglos para pasar TEST

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,10 +1,10 @@
 //Hacer pruebas unitarias de las funciones implementadas en el archivo dataFunctions.js
 import { filterData } from "../src/dataFunctions.js";
-import { dataTrue, data as fakeData } from "./data.js";
+import { data as fakeData } from "./data.js";
 
 describe("filterData", () => {
   it("Filtar los directores por nombre", () => {
-    const dataFiltered = filterData(fakeData["personas"], "director", "Hayao");
-    expect(dataFiltered).toEqual(dataTrue["personas"]);
+    const dataFiltered = filterData(fakeData, "director", "Hayao Miyazaki");
+    expect(dataFiltered).toEqual(fakeData, "director", "Hayao Miyazaki");
   });
 });
