3fffb38d17028aa2ac463146338efdd6b3fcff1e
Carolina <caroonava21@gmail.com>
2023-10-03 20:25:35 -0300

    diseño

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,10 +1,18 @@
 //Hacer pruebas unitarias de las funciones implementadas en el archivo dataFunctions.js
-import { filterData } from "../src/dataFunctions.js";
+import { filterData, computeStats } from "../src/dataFunctions.js";
 import { data as fakeData } from "./data.js";
 
 describe("filterData", () => {
   it("Filtar los directores por nombre", () => {
     const dataFiltered = filterData(fakeData, "director", "Hayao Miyazaki");
     expect(dataFiltered).toEqual(fakeData, "director", "Hayao Miyazaki");
+  });
+});
+
+//prueba Estadística
+describe("computeStats", () => {
+  it("Calcular el % de películas de cada director", () => {
+    const dataStats = computeStats(director, fakeData);
+    expect(dataStats).toEqual(director, fakeData);
   });
 });
